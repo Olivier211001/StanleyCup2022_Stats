01d3726aa5902d4e8905af5ebe3b47b123bad451
Olivier21 <olivier.lafleur21@gmail.com>
2022-06-02 14:12:39 -0400

    code bonifier

--- hockey-stats.py
+++ hockey-stats.py
@@ -12,52 +12,83 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 stats = []
 i = 0
+colPos = 0
+colPlayer = 1
+colTeam= 2
+colPosition = 3
+colGoals= 4
+colAssists = 5
+colPts = 6
+
 
 workbook = xlsxwriter.Workbook('stats.xlsx')
 worksheet = workbook.add_worksheet("Stanley_Cup_Series")
 
-worksheet.write("A1", "Order")
-worksheet.write("B1", "Player")
-worksheet.write("C1", "Team")
-worksheet.write("D1", "Pos")
-worksheet.write("E1", "G")
-worksheet.write("F1", "A")
-worksheet.write("G1", "Pts")
+#############################################
+
+def write_in_excel_file(row, col, content):
+        worksheet.write(row, col, content)
+
+def write_header():
+    worksheet.write("A1", "Order")
+    worksheet.write("B1", "Player")
+    worksheet.write("C1", "Team")
+    worksheet.write("D1", "Pos")
+    worksheet.write("E1", "G")
+    worksheet.write("F1", "A")
+    worksheet.write("G1", "Pts")
+
+def scrape_pos_player_team():
+    i = 0
+    for rows in soup.find_all("tr"):                    
+        player = rows.find('a', {'class': "AnchorLink"})
+        team = rows.find('span', {'class': "pl2 n10 athleteCell__teamAbbrev"})
+        position = rows.find('td', {'class': "Table__TD"})
+
+        if player is not None and player.get_text() != 'G':
+            write_in_excel_file(i, colPos, position.get_text())
+            write_in_excel_file(i, colPlayer, player.get_text())
+            write_in_excel_file(i, colTeam, team.get_text())
+
+        i = i + 1
+
+def scrape_position_andBuildStats():
+    i = 0
+    table = soup.find('table', {'class': "Table Table--align-right"})
+    for rows in table.find_all('tr'): 
+        pos = rows.find('td')   
+        datas = rows.find_all("td")[2:]
+        stats.append(datas)
+        if pos is not None:
+            write_in_excel_file(i, colPosition, pos.get_text())    
+        i = i + 1
+
+def write_Stats_in_excel():
+    i = 1
+    for data in stats:
+        if i < 51:
+            write_in_excel_file(i, colGoals, stats[i][0].get_text())
+            write_in_excel_file(i, colAssists, stats[i][1].get_text())
+            write_in_excel_file(i, colPts, stats[i][2].get_text())
+        i = i + 1
 
 
-for rows in soup.find_all("tr"):                    
-    player = rows.find('a', {'class': "AnchorLink"})
-    team = rows.find('span', {'class': "pl2 n10 athleteCell__teamAbbrev"})
-    position = rows.find('td', {'class': "Table__TD"})
-
-    if player is not None and player.get_text() != 'G':
-        worksheet.write(i, 0, position.get_text())
-        worksheet.write(i, 1, player.get_text())
-        worksheet.write(i, 2, team.get_text())
-
-    i = i +1
-i = 0
+################################################
 
 
-table = soup.find('table', {'class': "Table Table--align-right"})
-for rows in table.find_all('tr'): 
-    pos = rows.find('td')   
-    datas = rows.find_all("td")[2:]
-    stats.append(datas)
-    if pos is not None:
-        worksheet.write(i, 3, pos.get_text())    
-    i = i + 1
+write_header()
 
-i = 1
+scrape_pos_player_team()
 
-for data in stats:
-    if i < 51:
-        worksheet.write(i, 4, stats[i][0].get_text())
-        worksheet.write(i, 5, stats[i][1].get_text())
-        worksheet.write(i, 6, stats[i][2].get_text())
-    i = i + 1
+scrape_position_andBuildStats()
 
-workbook.close()   
+write_Stats_in_excel()
+
+workbook.close()
+
+
+
+
   
 
             
